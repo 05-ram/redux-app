b89f08569dd38839c480f0081f670401b6dd9bae
05-ram <karthik98@gmail.com>
2024-08-09 09:50:57 +0530

    commit

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { decrement, increment, reset, incrementByAmount } from './counterSlice';
 
 const Counter = () => {
-    const count = useSelector((state) => state.counter.count)
+    const count = useSelector((state) => state.counter.count);
     const dispatch = useDispatch();
+
+    const [incrementAmnt, setIncrementAmnt] = useState(0);
+    const addValue = Number(incrementAmnt) || 0;
+    const resetAll = () => {
+        setIncrementAmnt(0);
+        dispatch(reset());
+    }
     return (
         <section>
             <p className='txt'>{count}</p>
             <div className='d-flex g-20 jc mt-2'>
                 <button onClick={() => dispatch(increment())}>+</button>
                 <button onClick={() => dispatch(decrement())}>-</button>
+            </div>
+            <div className="mt-2">
+                <input type="text" value={incrementAmnt} onChange={(e) => setIncrementAmnt(e.target.value)} />
+            </div>
+            <div className="d-flex g-20 jc mt-2">
+                <button onClick={() => dispatch(incrementByAmount(addValue))}>Add Amount</button>
+                <button onClick={resetAll}>Reset</button>
             </div>
         </section>
     )
