7ab38bfa4a295c2293203f0e6162261b3b9cd4d7
ram <karthik@gmail.com>
2024-08-10 09:26:24 +0530

    updated style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Counter from './features/counter/Counter';
 import './global.css';
 
 function App() {
   return (
     <div className="App">
       <h2>Born To Achieve Great Things!</h2>
+      <Counter />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import './index.css';
 import App from './App';
 import { Store } from './app/store';
 import { Provider } from 'react-redux';
-=======
-import App from './App';
->>>>>>> d9f65583edf7a2500002b1a7fea6fca2b2ae92c6
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<<<<<<< HEAD
     <Provider store={Store}>
       <App />
     </Provider>
-=======
-    <App />
->>>>>>> d9f65583edf7a2500002b1a7fea6fca2b2ae92c6
   </React.StrictMode>
 );
