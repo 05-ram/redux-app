cb1e13b3e432799b98cce88efc110324be1a1f45
ram <karthik@gmail.com>
2024-08-20 22:59:36 +0530

    added addpostform basic func

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import AddPostForm from './components/AddPostForm';
 import PostList from './components/PostList';
 import './global.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>Born To Achieve Great Things!</h2>
+      {/* <h2>Born To Achieve Great Things!</h2> */}
+      <AddPostForm />
       <PostList />
     </div>
   );
--- src/components/AddPostForm.js
+++ src/components/AddPostForm.js
@@ -1,22 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { postAdded } from '../features/post/postSlice';
+import { nanoid } from '@reduxjs/toolkit';
+
 
 const AddPostForm = () => {
     const [skill, setSkill] = useState('');
     const [design, setDesign] = useState('');
-
-    const handleChange = e => setDesign(e.target.value);
+    const dispatch = useDispatch();
+    const handleChange = e => setSkill(e.target.value);
     const contentChange = e => setDesign(e.target.value);
+
+    const handleClick = () => {
+        if (skill && design) {
+            dispatch(
+                postAdded(
+                    { id: nanoid(), skill, design })
+            )
+        }
+    }
     return (
         <>
             <div>AddPostFrom</div>
-            <form>
-                <label htmlFor="skill">Enter the skill</label>
-                <input type="text" id='skill' name='skill' value={skill} onChange={handleChange} />
-                <label htmlFor="design">Enter the skill</label>
-                <input type="text" id='design' name='design' value={design} onChange={contentChange} />
+            <form className='post-form'>
+                <label htmlFor="skill">Enter the Skill</label><br />
+                <input type="text" id='skill' name='skill' value={skill} onChange={handleChange} className='w-100' /> <br /><br />
+
+                <label htmlFor="design">Enter the Design</label><br />
+                <input type="text" id='design' name='design' value={design} onChange={contentChange} className='w-100' /><br /><br />
+
+                <button type='button' onClick={handleClick} className='btn-green'>Save</button>
             </form>
         </>
     )
 }
 
-export default AddPostForm
+export default AddPostForm;
+
+
+// 2.28
